342c1a871f6b48b600e0680fab588340eadbc421
Laurence Dougal Myers <laurencedougalmyers@gmail.com>
2024-05-08 18:49:25 +1000

    VoiceOverFixAlternatives.cs: always reset state after hooking `GetIDOfConditionMetHook`, so it doesn't persist between attempted voice-over lookups. Exit early if there's no existing state (entry ID). Add extra logging, change some to debug level, add more info. Fix order of operations when resetting/setting state.

--- FuriousTareSteam/VoiceOverFixAlternatives.cs
+++ FuriousTareSteam/VoiceOverFixAlternatives.cs
@@ -21,14 +21,22 @@ public class VoiceOverFixAlternatives
     // ReSharper disable twice InconsistentNaming
     public static void GetIDOfConditionMetHook(ref int __result, DialogueEntry entry)
     {
+        Logger.Log.LogDebug(
+            $"JanusNode.GetIDOfConditionMet() called for {entry.id}. Original alternative ID: {__result}. Last entry ID: {_lastDialogueEntryId}. Last alternative: {_lastAlternativeId}"
+        );
+        if (_lastDialogueEntryId == null)
+        {
+            return;
+        }
         if (_lastDialogueEntryId == entry.id && _lastAlternativeId != null && __result != _lastAlternativeId)
         {
             Logger.Log.LogInfo(
-                $"JanusNode.GetIDOfConditionMet() intercepted for dialogue ID {entry.id}. Original alternative ID: {__result}, new ID: {_lastAlternativeId}"
+                $"JanusNode.GetIDOfConditionMet() intercepted for dialogue \"{Field.LookupValue(entry.fields, "Title")}\". Entry ID {entry.id}. Original alternative: {__result}, new alternative: {_lastAlternativeId}"
             );
             __result = _lastAlternativeId.Value;
-            ResetState();
         }
+        // Always reset state afterwards, so we don't accidentally match some other text/voice over pair.
+        ResetState();
     }
 
     public static void HandleEntryHook(DialogueEntry entry)
@@ -37,23 +45,23 @@ public class VoiceOverFixAlternatives
         SetState(
             entry
         );
-        Logger.Log.LogInfo(
+        Logger.Log.LogDebug(
             $"JanusNode.HandleEntry() intercepted. Title: {Field.LookupValue(entry.fields, "Title")}, ID: {_lastDialogueEntryId}, Articy ID: {Field.LookupValue(entry.fields, "Articy Id")}, dialogue alternative ID: {_lastAlternativeId}"
         );
     }
 
     private static void ResetState()
     {
-        _lastDialogueEntryId = null;
         _lastAlternativeId = null;
+        _lastDialogueEntryId = null;
     }
 
     private static void SetState(DialogueEntry entry)
     {
-        _lastDialogueEntryId = entry.id;
         _lastAlternativeId = JanusNode.GetIDOfConditionMet(
             entry
         );
+        _lastDialogueEntryId = entry.id;
     }
 
     public static void RegisterPatches()
