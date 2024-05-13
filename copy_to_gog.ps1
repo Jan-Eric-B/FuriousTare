f3a7078c856de8b569c8168967f6bd04ddd3b2d4
Laurence Dougal Myers <laurencedougalmyers@gmail.com>
2024-05-13 16:12:58 +1000

    Add Powershell script `copy_to_gog.ps1`

---
+++ copy_to_gog.ps1
@@ -0,0 +1,6 @@
+﻿Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
+Copy-Item -Path "FuriousTareIL2CPP\bin\Release\net6.0\FuriousTareIL2CPP.dll" -Destination "F:\Games\GOG\Disco Elysium\BepInEx\plugins\FuriousTare\"
+
+echo "Copied!"
