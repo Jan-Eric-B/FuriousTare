8b6e1767f125c10bd2866da022385109a75169ca
Laurence Dougal Myers <laurencedougalmyers@gmail.com>
2024-05-13 19:11:17 +1000

    StopWavingThatFlashlight: gradually "unsnap" the flashlight from the last target.
    This gives a pleasing ease back to following the cursor, rather than immediately snapping to it once a conversation is finished.

--- FuriousTareIL2CPP/Patches/StopWavingThatFlashlight.cs
+++ FuriousTareIL2CPP/Patches/StopWavingThatFlashlight.cs
@@ -1,5 +1,6 @@
 ﻿using FortressOccident;
 using HarmonyLib;
+using Il2CppSystem;
 using Sunshine.Metric;
 using UnityEngine;
 
@@ -8,7 +9,9 @@ namespace FuriousTareIL2CPP.Patches;
 [HarmonyPatch]
 public class StopWavingThatFlashlight
 {
+    private static readonly AnimationCurve FlashlightUnsnapRamp = AnimationCurve.EaseInOut(1f, 0.1f, 2, 5f);
     private static bool _isFlashlightIKEnabled = false;
+    private static DateTime? _lastToggleOnTime;
 
     [HarmonyPatch(
         typeof(Character),
@@ -18,10 +21,17 @@ public class StopWavingThatFlashlight
     public static void OnCharacterToggleFlashlightIK(ref bool __runOriginal, Character __instance, bool isEnabled)
     {
         Logger.Log.LogDebug(
-            $"Toggling character flashlight IK: {isEnabled}. (Skipping original method)"
+            $"Toggling character flashlight IK: {isEnabled}. (Replacing original method)"
         );
+        if (!_isFlashlightIKEnabled && isEnabled)
+        {
+            _lastToggleOnTime = DateTime.UtcNow;
+        }
+
         _isFlashlightIKEnabled = isEnabled;
-        if (InventoryViewData.Singleton.IsEquipped("flashlight"))
+        if (InventoryViewData.Singleton.IsEquipped(
+                "flashlight"
+            ))
         {
             try
             {
@@ -34,10 +44,12 @@ public class StopWavingThatFlashlight
             }
             catch
             {
-                Debug.Log("Unable to toggle flashlight IK because flashlight is hidden");
+                Debug.Log(
+                    "Unable to toggle flashlight IK because flashlight is hidden"
+                );
             }
         }
-        
+
         __runOriginal = false;
     }
 
@@ -46,7 +58,7 @@ public class StopWavingThatFlashlight
         nameof(FlashlightTargetBehaviour.Update)
     )]
     [HarmonyPrefix]
-    public static void OnFlashlightTargetBehaviourUpdate(ref bool __runOriginal)
+    public static void OnFlashlightTargetBehaviourUpdate(ref bool __runOriginal, FlashlightTargetBehaviour __instance)
     {
         if (!_isFlashlightIKEnabled)
         {
@@ -55,6 +67,29 @@ public class StopWavingThatFlashlight
             //     $"Flashlight IK was disabled, so don't update the IK target"
             // );
             __runOriginal = false;
+        }
+        else if (_lastToggleOnTime != null)
+        {
+            // Ease the IK back into the world for a little bit. Avoids a quick "snap" back to the mouse cursor.
+            var lastTime = _lastToggleOnTime.Value;
+            var now = DateTime.UtcNow;
+            var diff = (now - lastTime).TotalSeconds;
+            if (diff <= 1)
+            {
+                __instance.maxSpeed = 0.1f;
+            }
+            else if (diff <= 2)
+            {
+                __instance.maxSpeed = FlashlightUnsnapRamp.Evaluate((float)diff);
+            }
+            else
+            {
+                // Logger.Log.LogInfo(
+                //     "Resetting Flashlight IK max speed"
+                // );
+                __instance.maxSpeed = 10f;
+                _lastToggleOnTime = null;
+            }
         }
     }
 }
